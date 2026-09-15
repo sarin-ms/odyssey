349439b0b224d631fad4aa7e7aa28e3f8729311e
Sarin <sarinms777@gmail.com>
2026-09-15 18:36:41 +0530

    feat: add register redirect

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/register",
+        destination: "https://makemypass.com/event/odyssey",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
